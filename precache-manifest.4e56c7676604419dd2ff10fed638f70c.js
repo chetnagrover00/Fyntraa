ebc41111bc60658f2f65b5040d02f4e0f4864ae0
chetnagrover00 <65505815+chetnagrover00@users.noreply.github.com>
2021-08-03 23:38:02 +0530

    Updates

---
+++ precache-manifest.4e56c7676604419dd2ff10fed638f70c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc38776b8c95dfd1af5699228a23061f",
+    "url": "/Fyntraa/index.html"
+  },
+  {
+    "revision": "1bf4801552d8e489fbfd",
+    "url": "/Fyntraa/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "04241fffb509400506f8",
+    "url": "/Fyntraa/static/css/main.6f373500.chunk.css"
+  },
+  {
+    "revision": "1bf4801552d8e489fbfd",
+    "url": "/Fyntraa/static/js/2.e675352e.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/Fyntraa/static/js/2.e675352e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04241fffb509400506f8",
+    "url": "/Fyntraa/static/js/main.db2f5f54.chunk.js"
+  },
+  {
+    "revision": "9b622cdd0c7918832f6a",
+    "url": "/Fyntraa/static/js/runtime-main.1aef1c5f.js"
+  },
+  {
+    "revision": "83818c20163663b6a59c79adb01d4c70",
+    "url": "/Fyntraa/static/media/Fashion.83818c20.svg"
+  },
+  {
+    "revision": "aceebdc45c2e24eda7c713bf39cd7ffa",
+    "url": "/Fyntraa/static/media/Shoot.aceebdc4.svg"
+  }
+]);
